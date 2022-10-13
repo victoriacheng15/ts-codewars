f9c7e051db85f5f798361c5d80f8265a72fec4da
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-10-13 12:28:41 -0600

    feat: add problem description (#2)
    * feat: add problem description
    
    * feat: add problem description and remove the first line

--- 7kyu/square-every-digit.test.ts
+++ 7kyu/square-every-digit.test.ts
@@ -1,4 +1,8 @@
-import { describe, expect, it } from '@jest/globals';
+/* 
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+*/
 
 export class Kata {
   static squareDigits(num: number): number {
