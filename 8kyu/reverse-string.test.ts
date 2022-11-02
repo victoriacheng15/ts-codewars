1e770a68238e39666b03e8f1f7bb09dde99ee14e
Victoria Cheng <victoria.cheng15@gmail.com>
2022-11-02 13:11:25 -0600

    feat: add reverse string solution with jest

---
+++ 8kyu/reverse-string.test.ts
@@ -0,0 +1,28 @@
+/* 
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+export function solution(str: string): string {
+  return [...str].reduce((output, char) => {
+    output = char + output;
+    return output;
+  }, '');
+}
+
+describe('reverse string', () => {
+  const cases: Array<string[]> = [
+    ['world', 'dlrow'],
+    ['hello', 'olleh'],
+    ['word', 'drow'],
+  ];
+
+  for (const [str, output] of cases) {
+    it(`should return ${output} when str = ${str}`, () => {
+      const input = solution(str);
+      expect(input).toBe(output);
+    });
+  }
+});
